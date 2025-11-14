f639dfe61fd1de9636bf703279e5d1fc4df3706b
Attila Farago <attila.farago.hu@gmail.com>
2025-11-14 22:25:41 +0100

    refactor: streamline device detection logic in hub-autodetect.py

--- asset/python-libs/hub-autodetect.py
+++ asset/python-libs/hub-autodetect.py
@@ -5,12 +5,8 @@ Detects and lists all connected devices on a SPIKE Prime, RI, Technic or City Hu
 Returns: List of [port, device_type_id] pairs
 """
 
-from pybricks.hubs import ThisHub
-from pybricks.iodevices import PUPDevice
-from pybricks.parameters import Port
-detect = []; hub = ThisHub
-for pc in dir(Port):
-  try: port = getattr(Port,pc); detect.append([pc,PUPDevice(port).info()['id']])
-  except: detect.append([pc,0])
-  
-print("AUTODETECT", detect)
+from pybricks.parameters import Port; from pybricks.iodevices import PUPDevice
+def gp(pc):
+  try: return PUPDevice(getattr(Port,pc)).info()['id']
+  except: return 0
+print('AUTODETECT', [[pc,gp(pc)] for pc in dir(Port)])
